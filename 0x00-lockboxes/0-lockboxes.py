4ee373d46acf41b8b52466323b44b5929ce40abe
Acochisse <1819@holbertonschool.com>
2022-08-30 08:07:23 -0500

    adding task files

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""a function that unlocks all boxes"""
+
+
+def canUnlockAll(boxes):
+    """a function that unlocks all boxes"""
+    keys = [0]
+    for key in keys:
+        for key2 in boxes[key]:
+            if key2 not in keys and key2 < len(boxes):
+                keys.append(key2)
+    if len(keys) == len(boxes):
+        return True
+    return False
