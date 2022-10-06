31e6c52fb7921909b4a7eacfba165da8640bb440
Acochisse <1819@holbertonschool.com>
2022-10-06 18:18:44 -0500

    added a separate add function and made sum run the calls. works locally now

--- 0x03-sandpiles/0-sandpiles.c
+++ 0x03-sandpiles/0-sandpiles.c
@@ -9,48 +9,31 @@
  */
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+
 {
-	int i, j, unstable, TempG[3][3];
-
-	for (i = 0; i < 3; i++)
-		for (j = 0; j < 3; j++)
-			TempG[i][j] = grid1[i][j] + grid2[i][j];
-
-
-	while (!eval(TempG))
-	{
-		unstable = eval(TempG);
-			if (unstable == 1)
-			{
-				printf("=\n");
-				print_grid(TempG);
-				topple(TempG);
-			}
-	}
-
-	if (unstable == 0)
+	grid_add(grid1, grid2);
+	while (!eval(grid1))
 	{
 		printf("=\n");
-		print_grid(TempG);
+		print_grid(grid1);
+		topple(grid1);
 	}
 }
-
 /**
  * eval - determines if a sandpile is stable or not
  * @TempG - the sandpile to be evaled
  * 
  * Return: 1 if the sandpile is good, 0 if not
  */
-int eval(int TempG[3][3])
+int eval(int grid1[3][3])
 {
 	int x, y;
 
 	for (x = 0; x < 3; x++) {
 		for (y = 0; y < 3; y++)
 		{
-			if (TempG[x][y] > 3)
-			{
-				
+			if (grid1[x][y] > 3)
+			{	
 				return (0);
 			}
 		}
@@ -65,31 +48,44 @@ int eval(int TempG[3][3])
  *
  * Return: Stable grid1 
  */
-int topple(int TempG[3][3])
+void topple(int grid1[3][3])
 {
-	int i, j, outgrid[3][3];
-	for (i = 0; i < 3; i++)
-	{
-		for (j = 0; j < 3; j++)
-		{
-			outgrid[i][j] = TempG[i][j];
+	int x, y;
+	int TempG[3][3];
 
-			if (TempG[i][j] > 3)
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			TempG[x][y] = grid1[x][y];
+		}
+	}
+	for (x = 0; x < 3; x++)
+	{
+		for (y = 0; y < 3; y++)
+		{
+			if (TempG[x][y] > 3)
 			{
-				outgrid[i][j] = TempG[i][j] - 4;
-				if ((i + 1) < 3)
-					outgrid[i + 1][j] += 1;
-				if ((i - 1) <= 0)
-					outgrid[i - 1][j] += 1;
-				if ((j + 1) < 3)
-					outgrid[i][j + 1] += 1;
-				if ((j - 1) >= 0)
-					outgrid[i][j - 1] += 1;
+				grid1[x][y] -= 4;
+				if (x - 1 >= 0)
+					grid1[x - 1][y] += 1;
+				if (x + 1 < 3)
+					grid1[x + 1][y] += 1;
+				if (y - 1 >= 0)
+					grid1[x][y - 1] += 1;
+				if (y + 1 < 3)
+					grid1[x][y + 1] += 1;
 			}
 		}
-	} return (outgrid[3][3]);
+	}
 }
 
+/**
+ * print_grid - prints a 3x3 grid
+ * @grid: 3x3 grid
+ *
+ * Return: void
+ */
 static void print_grid(int grid[3][3])
 {
     int i, j;
@@ -104,4 +100,25 @@ static void print_grid(int grid[3][3])
         }
         printf("\n");
     }
+}
+
+/**
+ * grid_add - adds two sandpiles by the rules of sandpiles
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ * Return: Stable grid1 
+ */
+
+void grid_add(int grid1[3][3], int grid2[3][3])
+
+{
+	int x, y;
+
+	for (x = 0; x < 3; x++) {
+		for (y = 0; y < 3; y++)
+		{
+			grid1[x][y] = grid1[x][y] + grid2[x][y];
+		}
+	}
 }
