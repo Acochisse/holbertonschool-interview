ecc7819622be704338bf6a1bc00be5154c4b5bd4
Acochisse <1819@holbertonschool.com>
2022-10-06 22:33:37 -0500

    updating sandpiles header

--- 0x03-sandpiles/sandpiles.h
+++ 0x03-sandpiles/sandpiles.h
@@ -7,9 +7,10 @@
 #include <math.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-int eval(int tempG[3][3]);
-int topple(int grid1[3][3]);
+int eval(int grid1[3][3]);
+void topple(int grid1[3][3]);
 static void print_grid(int grid[3][3]);
+void grid_add(int grid1[3][3], int grid2[3][3]);
 
 
 
