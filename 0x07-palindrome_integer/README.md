0a1b75c3410257cd8a84f81fb2b60909e4db85bb
Acochisse <1819@holbertonschool.com>
2022-10-26 19:59:23 -0500

    adding files for int palindrome program

---
+++ 0x07-palindrome_integer/README.md
@@ -0,0 +1,3 @@
+This Directory is related to project 1699 - 0x07-palindrome_integer
+
+## 0\. Palindrome unsigned integer
---
+++ 0x07-palindrome_integer/palindrome.h
@@ -0,0 +1,15 @@
+#ifndef _PALINDROME_H_
+#define _PALINDROME_H_
+
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+
+/* prototypes */
+
+int is_palindrome(unsigned long n);
+
+#endif
