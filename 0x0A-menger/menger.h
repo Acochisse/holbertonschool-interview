45096d48f2b3bd236eb520b25619f495a5027380
Blake Stewart <1819@holbertonstudents.com>
2022-12-07 16:12:40 -0600

    adding task files

---
+++ 0x0A-menger/menger.h
@@ -0,0 +1,13 @@
+#ifndef _MENGER_H_
+#define _MENGER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <math.h>
+
+void menger(int level);
+
+#endif /* _MENGER_H_ */
