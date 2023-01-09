16ae6cc0e364a0e170e3e33598c8271955c5c966
Acochisse <1819@holbertonschool.com>
2023-01-09 10:50:06 -0600

    pycodestyle fixes

--- 0x00-rain/0-rain.py
+++ 0x00-rain/0-rain.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
-Given a list of non-negative integers representing the heights of walls with unit width 1, as if viewing the cross-section of a relief map, calculate how many square units of water will be retained after it rains.
+Given a list of non-negative integers representing the heights of walls with
+unit width 1, as if viewing the cross-section of a relief map, calculate how
+many square units of water will be retained after it rains.
 """
 
+
 def rain(walls):
-    """finds the lowest wall relative to the index of the water and returns that as the water level"""
+    """finds the lowest wall relative to the index of the water and returns
+    that as the water level"""
     if len(walls) <= 1:
         return 0
-    
     water = 0
     for i in range(len(walls)):
         if walls[i] != max(walls):
