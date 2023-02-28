d45ede0c8a54f731e97ad4bf0a02ac7cd054ba1a
Acochisse <1819@holbertonschool.com>
2023-02-28 09:37:25 -0600

    adding making change

---
+++ 0x0F-making_change/0-making_change.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Module file for makeChange
+"""
+import sys
+
+
+def makeChange(coins, total):
+    """
+    Function that determines the fewest number of coins needed to meet a given
+    amount total
+    """
+    if total <= 0:
+        return 0
+
+    coins.sort(reverse=True)
+    count = 0
+    for coin in coins:
+        if total == 0:
+            return count
+        if coin <= total:
+            count += total // coin
+            total = total % coin
+
+    if total != 0:
+        return -1
+    return count
---
+++ 0x0F-making_change/README.md
@@ -0,0 +1,1 @@
+This directory is related to 0x0F-making_change
